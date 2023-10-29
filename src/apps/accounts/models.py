2f7ebc65d6665bf741cde0ca6606a2be63e558a0
emok1d <148631609+emok1d@users.noreply.github.com>
2023-10-29 21:21:14 +0400

    Update models.py

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
@@ -11,19 +9,5 @@ class UserProfile(models.Model):
     balance = models.FloatField()
     bonus = models.FloatField()
 
-    class Meta:
-        db_table = "Users"
-
     def __str__(self) -> str:
         return self.user.username
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance, age=0, balance=0, bonus=0)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
