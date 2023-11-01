669925ce9cd126672a40a4197d7cd6feb3dc2680
AzorRose <imrmn.rkv@gmail.com>
2023-11-01 19:43:52 +0400

    +slug filter for event view

--- src/apps/events/views.py
+++ src/apps/events/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Event
 from django.views import View
-
+from re import findall
 
 # Create your views here.
 class EventView(View):
     def get(self, request, *args, **kwargs):
-        event = Event.objects.all()
+        path = request.path
+        slug_match = path[path.rfind('/')+1:]
+        event = Event.objects.filter(slug=slug_match)
         return render(request, "events/index.html", context={"event": event})
