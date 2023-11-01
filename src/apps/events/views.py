c766f41da7e5c3e171ae9ffa5c74c06e5fa55df8
AzorRose <imrmn.rkv@gmail.com>
2023-11-01 19:47:16 +0400

    +comment

--- src/apps/events/views.py
+++ src/apps/events/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import Event
 from django.views import View
-from re import findall
 
 # Create your views here.
 class EventView(View):
     def get(self, request, *args, **kwargs):
-        path = request.path
-        slug_match = path[path.rfind('/')+1:]
+        # Получает путь из запроса и возвращает информацию о конкретном событии, которому соответствует этот путь
+        slug_match = request.path[request.path.rfind('/')+1:]
         event = Event.objects.filter(slug=slug_match)
-        return render(request, "events/index.html", context={"event": event})
+        if event:
+            return render(request, "events/index.html", context={"event": event})
