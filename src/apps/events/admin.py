4823c4f6c1f4bcc6e9347a9420b71b147b4981ef
AzorRose <imrmn.rkv@gmail.com>
2023-11-04 14:13:47 +0400

    +delete image on event delete

--- src/apps/events/admin.py
+++ src/apps/events/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Event, Ticket
 
+
 # Register your models here.
 class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
-    
-    
+
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(Ticket)
--- src/apps/events/views.py
+++ src/apps/events/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from .models import Event
 from django.views import View
 
+
 # Create your views here.
 class EventView(View):
     def get(self, request, *args, **kwargs):
         # Получает путь из запроса и возвращает информацию о конкретном событии, которому соответствует этот путь
-        slug_match = request.path[request.path.rfind('/')+1:]
+        slug_match = request.path[request.path.rfind("/") + 1 :]
         event = Event.objects.filter(slug=slug_match)
         if event:
             return render(request, "events/index.html", context={"event": event})
