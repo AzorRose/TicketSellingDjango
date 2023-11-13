f8b3bc06a2bc73b34689579bc2dc10223aa4d12e
emok1d <148631609+emok1d@users.noreply.github.com>
2023-11-13 14:01:03 +0400

    Update views.py

--- src/apps/events/views.py
+++ src/apps/events/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .models import Event
+from apps.accounts.models import UserProfile
 from django.views import View
 
 
 # Create your views here.
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        event = Event.objects.all()
+        profile = UserProfile.objects.all()
+        ticket = Ticket.objects.all()
+        return render(request, "events/index.html", context={"profile": profile, "event": event, "ticket": ticket})
+
+
 class EventView(View):
     def get(self, request, *args, **kwargs):
         # Получает путь из запроса и возвращает информацию о конкретном событии, которому соответствует этот путь
