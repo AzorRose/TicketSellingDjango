d7536c1436bcf2e8920e5000261a7c296750e3a4
emok1d <148631609+emok1d@users.noreply.github.com>
2023-11-14 23:14:58 +0400

    Update views.py

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -78,7 +78,7 @@ class ProfileView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["username"] = self.request.user.username
-        context["first_name"] = self.request.user.first_name
-        #context["second_name"] = self.request.user.second_name
-        #context["balance"] = self.request.user.balance
+        context["first_name"] = self.request.user.profile.first_name
+        context["second_name"] = self.request.user.profile.second_name
+        context["balance"] = self.request.user.profile.balance
         return context
