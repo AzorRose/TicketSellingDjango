370ea00c455d2d52953c83f81a4711ad29f60d47
AzorRose <imrmn.rkv@gmail.com>
2023-11-26 15:23:37 +0400

    +buildings&areas & minor fixes

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import UserProfile, Purchase
 
 class PurchaseAdmin(admin.ModelAdmin):
-    readonly_fields = ('user', 'ticket', 'creation_time', )
+    readonly_fields = ('creation_time', )
 
 
 admin.site.register(UserProfile)
