d9702021e484e9934a73f8043d93ee3789d1de99
AzorRose <imrmn.rkv@gmail.com>
2023-12-09 17:49:51 +0400

    +libs

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER-PC\Downloads\rrr\rrr\TicketSellingDjango\.venv
+set VIRTUAL_ENV=D:\rrr\rrr\TicketSellingDjango\TicketSellingDjango\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
