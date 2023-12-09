a5f99ce2d85d87c6835dc74c4a0a31aee28692c9
AzorRose <imrmn.rkv@gmail.com>
2023-12-09 21:52:59 +0400

    +basket, fix profile request

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from apps.events.models import Ticket, Booked_Places
 from django.utils import timezone
 from datetime import date
+from django.contrib.postgres.fields import ArrayField
 
 
 class UserProfile(models.Model):
@@ -10,13 +11,15 @@ class UserProfile(models.Model):
         User, null=True, on_delete=models.CASCADE, related_name="profile"
     )
 
-    first_name = models.CharField(max_length=32, blank=True)
-    second_name = models.CharField(max_length=32, blank=True)
-    gender = models.CharField(max_length=9, blank=True)
+    first_name = models.CharField(max_length=32, blank=True, default="")
+    second_name = models.CharField(max_length=32, blank=True, default="")
+    gender = models.CharField(max_length=9, blank=True, default="")
     birth_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
     balance = models.FloatField(default=0)
     bonus = models.FloatField(default=0)
     buyback_sum = models.FloatField(default=0)
+
+    basket = ArrayField(base_field=models.CharField(default=""), blank=True, null=True)
 
     bonus_levels = {5000: 0, 10000: 2, 30000: 5, float("inf"): 10}
 
@@ -32,9 +35,9 @@ class UserProfile(models.Model):
         for i in self.bonus_levels:
             if self.buyback_sum < i:
                 self.bonus = self.bonus_levels[i]
+                self.save(update_fields=["bonus"])
                 break
-        self.save(update_fields=["bonus"])
-
+        
     @property
     def age(self):
         today = date.today()
@@ -43,13 +46,49 @@ class UserProfile(models.Model):
             - self.birth_date.year
             - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
         )
+    
+    @property
+    def get_basket(self) -> list:
+        return [Purchase.objects.get(id = id) for id in self.basket]
+    
+    @property
+    def basket_sum(self) -> float:
+        total = sum([i.ticket.price for i in self.get_basket])
+        return total - total * (self.bonus / 100) if self.bonus != 0 else total
+    
+    @property
+    def have_money(self):
+        return self.balance >= self.basket_sum
 
+    def buy(self):
+        if self.have_money:
+            for i in self.get_basket:
+                if i.have_place:
+                    # Подтвердить покупку
+                    i.make_completed()
+                    # Изменение общего количества людей
+                    i.ticket.event.change_people_count(True)
+                    # Изменение количества людей на конкретный тип места
+                    i.ticket.event.change_spot_count(i.ticket.spot, True)
+                    # Бронь конкретного места
+                    temp = Booked_Places.objects.get(
+                            spot_num=i.spot_num, spot=i.ticket.spot
+                        )
+                    temp.available = False
+                    temp.user = self
+                    temp.save()
+                    # Установка времени покупки билета
+                    i.creation_time = timezone.now()
+                    super(Purchase, i).save()
+            
+        self.balance -= self.basket_sum
+        self.basket.clear()
+        self.save(update_fields=["balance", "basket"])
+        self.count_buyback()
+            
     def add_balance(self, num):
         self.balance += num
         self.save(update_fields=["balance"])
-
-    def get_balance(self):
-        return self.balance
 
     def __str__(self) -> str:
         return self.user.username
@@ -77,7 +116,14 @@ class Purchase(models.Model):
  
     spot_num = models.IntegerField(default=0)
 
-    def can_buy_ticket(self) -> bool:
+    completed = models.BooleanField(default=False)
+
+    def add_to_basket(self):
+        if self.have_place():
+            self.user.basket.append(self.id)
+            self.user.save(update_fields=["basket"])
+
+    def have_place(self) -> bool:
         has_place = (
             True
             if (
@@ -98,52 +144,22 @@ class Purchase(models.Model):
             else False
         )
 
-        has_money = (
-            True
-            if self.user.get_balance()
-            >= self.ticket.price - (self.ticket.price * (self.user.bonus / 100))
-            else False
-        )
-
         is_available = Booked_Places.objects.get(
             spot_num=self.spot_num, spot=self.ticket.spot
         ).available
 
-        return has_money and has_place and is_available
+        return has_place and is_available
 
-    def buy_ticket(self):
-        self.user.balance -= self.ticket.price - (
-            self.ticket.price * (self.user.bonus / 100)
-        )
-        self.user.save(update_fields=["balance"])
+    def make_completed(self):
+        self.completed = True
+        self.save(update_fields=["completed"])
 
     def save(self, *args, **kwargs):
         if not self.id:
-            if self.can_buy_ticket():
-                # Вычет баланса
-                self.buy_ticket()
-                # Изменение общего количества людей
-                self.ticket.event.change_people_count(True)
-                # Изменение количества людей на конкретный тип места
-                self.ticket.event.change_spot_count(self.ticket.spot, True)
-                # Бронь конкретного места
-                temp = Booked_Places.objects.get(
-                    spot_num=self.spot_num, spot=self.ticket.spot
-                )
-                temp.available = False
-                temp.user = self.user
-                temp.save()
-                # Установка времени покупки билета
-                self.creation_time = timezone.now()
-                print(self.creation_time, *args, **kwargs)
-                super(Purchase, self).save(*args, **kwargs)
-                # Подсчет новой суммы выкупа пользователя для получения скидки
-                self.user.count_buyback()
-                return
-            else:
-                raise Exception("can`t buy it")
+            super(Purchase, self).save(*args, **kwargs)
+            self.add_to_basket()
         super(Purchase, self).save(*args, **kwargs)
-
+    
     def delete(self, *args, **kwargs):
         self.ticket.event.change_people_count(False)
         super(Purchase, self).delete(*args, **kwargs)
